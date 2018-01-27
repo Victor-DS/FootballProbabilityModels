965d3ca20d18e50e5f3a75fd90009e176d21b33d
Victor Santiago <victords@amazon.com>
2018-01-27 14:51:07 -0200

    JsonUtil class to deal with parsing

---
+++ src/test/java/victor/santiago/soccer/poisson/util/JsonUtilTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2018 victords
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package victor.santiago.soccer.poisson.util;
+
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import victor.santiago.soccer.poisson.model.League;
+
+public class JsonUtilTest {
+
+    private static final String SAMPLE_JSON = "";
+
+    private JsonUtil toTest = new JsonUtil();
+
+    @Test
+    public void shouldParseList() throws IOException {
+        List<League> result = toTest.getLeagues("src/test/resources/sample.json");
+        Assert.assertEquals(2, result.size());
+        Assert.assertEquals("Palmeiras", result.get(0).getChampion());
+    }
+
+    @Test(expected = NoSuchFileException.class)
+    public void shouldNotFindFile() throws Exception {
+        toTest.getLeagues("invalid_path.json");
+    }
+}
